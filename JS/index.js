d06cf9ed7c9a88ebe09ebc00e97c4b47c806f152
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2023-06-27 10:41:18 +0530

    new

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,23 @@ let navbar = document.querySelector(".navbar");
 menu.onclick = () => {
     navbar.classList.toggle("open");
 }
+
+// SCROLL-SECTION ACTIVE LINK //
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active');
+        } else {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active');
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
